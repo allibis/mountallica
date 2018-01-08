071b06030b4ed2ce4380931b1c9bc79bee101f8b
allibis <34099018+allibis@users.noreply.github.com>
2018-01-08 19:09:56 +0100

    Update aurdown.sh

--- aurdown/aurdown.sh
+++ aurdown/aurdown.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-cd ~/
+sudo pacman -S $1
 
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
-tar -xf $1.tar.gz
-cd $1/
-makepkg -sic
 
+for errore in stdout
+
+do
+        echo '.......................................'
+        echo '==> package not found in the official repos'
+        echo '==> searching it in the AUR'
+        echo '.......................................'
+        cd $HOME
+        mkdir $1
+        cd $HOME/$1
+        wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
+        tar -xf $1.tar.gz
+        makepkg -sic
+done
+~      
 
