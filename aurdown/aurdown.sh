e04649fcef0917b4a7141b6c699ac3b5cefc35b8
allibis <34099018+allibis@users.noreply.github.com>
2018-01-09 20:42:31 +0100

    Update aurdown.sh

--- aurdown/aurdown.sh
+++ aurdown/aurdown.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 if ! pacman -Si $1 >&/dev/null; then
-        cd $HOME
-        if ping -c1 https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz >&/dev/null; then
-                echo '+-------------------------------------------+'
-                echo '|==> package not found in the official repos|'
-                echo '|==> searching it in the AUR                |'
-                echo '+-------------------------------------------+'
-                mkdir $1
-                cd $HOME/$1
-                wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
-                tar -xf $1.tar.gz
-                makepkg -sic
-                echo 'installation complete'
+        cd $HOME                
+        echo '+-------------------------------------------+'
+        echo '|==> package not found in the official repos|'
+        echo '|==> searching it in the AUR                |'
+        echo '+-------------------------------------------+'
+        mkdir $1
+        cd $HOME/$1
+        if ! wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz >&/dev/null; then
+                echo '==> package not found'
         else
-                echo 'package does exist'
+                tar -xf $1.tar.gz
+                cd $1
+                if ! makepkg -sic; then                 
+                        echo '==> installation failed'
+                else
+                        echo '==> installation complete'
+                fi
         fi
-else
+        rm -r $HOME/$1
+else    
         sudo pacman -S $1
 fi
 exit
