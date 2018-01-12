fe8ad7bb6430081e300a06e6ed566e6ed859f89c
allibis <34099018+allibis@users.noreply.github.com>
2018-01-12 22:37:53 +0100

    Update aurdown.sh

--- aurdown/aurdown.sh
+++ aurdown/aurdown.sh
@@ -9,7 +9,18 @@ if ! pacman -Si $1 >&/dev/null; then
         mkdir $1
         cd $HOME/$1
         if ! wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz >&/dev/null; then
-                echo '==> package not found'
+                if ! wget https://aur.archlinux/cgit/aur.git/snapshot/$1-git.tar.gz >&/dev/null ; then
+                        echo '==> package not found'
+		else
+			tar -xf $1-git.tar.gz
+			cd $1
+			if ! makepkg -sic ; then
+				echo '==> installation failed'
+			else
+				echo '==> installation complete'
+			fi
+		fi
+
         else
                 tar -xf $1.tar.gz
                 cd $1
